4618f46720d2777f81aafebac397e9d7ab85cc3b
--list <{azahmed@gmail.com}>
2026-05-17 17:02:41 +0300

    Added view to manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,7 @@
         'security/ir.model.access.csv',
         'data/estate_property_data.xml',
         'views/estate_property_type_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
