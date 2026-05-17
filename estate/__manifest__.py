c112c3e6a29c9b480410eb9f7f9160d9c1f48627
--list <{azahmed@gmail.com}>
2026-05-17 10:21:53 +0300

    Fixed security file name, has to be model.access and updated mainfest/secirity/*

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,7 +3,7 @@
     'depends': ['base'],
     'application': True,
     'data': [
-        'security/ir.models.access.csv',
+        'security/ir.model.access.csv',
         'data/estate_property_data.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
