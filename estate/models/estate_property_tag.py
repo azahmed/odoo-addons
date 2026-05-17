45cebc484ef33726b6386f43ed863477350711ef
--list <{azahmed@gmail.com}>
2026-05-17 14:48:49 +0300

    Adding Tag model

---
+++ estate/models/estate_property_tag.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+class EstatePropertyTag(models.Model):
+    _name = "estate_property_tag"
+    _description = "Estate Property Tag"
+
+    name = fields.Char(required=True)
+
+    #property_ids = fields.One2many('estate.property', 'property_type_id', string='Materials')
