b52b4f69397e450a5b30fe8154b8412852763f2c
--list <{azahmed@gmail.com}>
2026-05-19 15:19:32 +0300

    Added validity

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -19,10 +19,17 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one("res.partner", string="Partner")
     property_id = fields.Many2one("estate_property", string="Property", required=True)
     validity = fields.Integer(default=7)
-    date_deadline = fields.Date(inverse="_inverse_date_deadline")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
     # date_deadline = fields.Date()
 
     @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date is None:
+                record.date_deadline = today + record.validity
+            else:
+                record.date_deadline = record.create_date + record.validity
+
     def _inverse_date_deadline(self):
         for record in self:
             if record.create_date is None:
