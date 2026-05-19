a5b00f1d202725c98479f899c15a2fb79638c906
--list <{azahmed@gmail.com}>
2026-05-19 14:56:16 +0300

    Added validity

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from email.policy import default
 
 from odoo import fields, models
 from pkg_resources import require
+today = fields.Datetime.now()
 
 
 class EstatePropertyOffer(models.Model):
@@ -16,5 +18,13 @@ class EstatePropertyOffer(models.Model):
         help="Refused or Accepted")
     partner_id = fields.Many2one("res.partner", string="Partner")
     property_id = fields.Many2one("estate_property", string="Property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_inverse_date_deadline")
 
-    #property_ids = fields.One2many('estate.property', 'property_type_id', string='Materials')
+    @api.depends("create_date", "validity")
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date is None:
+                record.date_deadline = today + record.validity
+            else:
+                record.date_deadline = record.create_date + record.validity
