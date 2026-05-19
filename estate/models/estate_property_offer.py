8a646befce487ec95f5487417a8cea10892a822a
--list <{azahmed@gmail.com}>
2026-05-19 09:43:44 +0300

    Added partner_id

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -2,6 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from pkg_resources import require
+
 
 class EstatePropertyOffer(models.Model):
     _name = "estate_property_offer"
@@ -12,7 +14,7 @@ class EstatePropertyOffer(models.Model):
         string='Status',
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         help="Refused or Accepted")
-    # partner_id = fields.Many2one("res.partner", string="Partner")
-    property_id = fields.Many2one("estate_property", string="Property")
+    partner_id = fields.Many2one("res.partner", string="Partner")
+    property_id = fields.Many2one("estate_property", string="Property", required=True)
 
     #property_ids = fields.One2many('estate.property', 'property_type_id', string='Materials')
