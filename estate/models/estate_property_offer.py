aeb81190b9f05a09ee31891351006c847da45d67
--list <{azahmed@gmail.com}>
2026-05-20 13:58:18 +0300

    Added error

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -39,13 +39,13 @@ class EstatePropertyOffer(models.Model):
     def action_estate_offer_confirm(self):
         for record in self:
             self.property_id.selling_price = self.price
-            self.status = 'accepted'
+            self.status = 'accepted' # no 'Accepted'
             # raise UserError("Confirming Mate")
 
         return True
 
     def action_estate_offer_cancel(self):
         for record in self:
-            self.status = 'refused'
+            self.status = 'refused' # no 'Refused'
 
         return True
