0859a5791905d83e6a58233b522bae9bf54d3b17
--list <{azahmed@gmail.com}>
2026-05-21 13:28:05 +0300

    Constraints:

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -90,7 +90,7 @@ class EstateProperty(models.Model):
     @api.constrains('selling_price')
     def _check_selling_price(self):
         for record in self:
-            ValidationError("Constraint Man")
+            raise ValidationError("Constraint Man")
             if float_utils.float_is_zero(record.selling_price, precision_digits=2):
                 raise ValidationError("Selling Price cannot be zero")
 
